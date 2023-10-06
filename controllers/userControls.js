cd9ea819e03842a9ef16a8ed5d5f63df56341c12
shubhamrpgupta <144988807+shubhamrpgupta@users.noreply.github.com>
2023-10-06 19:12:43 +0530

    Updated userControls.js

--- controllers/userControls.js
+++ controllers/userControls.js
@@ -26,23 +26,19 @@ module.exports.renderLogInUser = (req, res) => {
     res.render('users/login');
 }
 // In the POST /login route (in the routes/users.js file), call the storeReturnTo middleware function before passport.authenticate() or passportMiddlewareToLogin.
-//Remember that middleware functions are executed in the order they are specified in the route. 
-//So, in this case, storeReturnTo should be called first, followed by passport.authenticate() and then the final middleware function to redirect the user.
 module.exports.logInUserAndGetBackToOriginalUrl = (req, res) => {
     req.flash('success', 'WELCOME BACK!!')
     const redirectUrl = res.locals.returnToUrlBeforeLogIn || '/campgrounds';  //if user has been logged in already then we'll be redirecting the user to the '/campgrounds'
-    res.redirect(redirectUrl);  //we're redirecting the user to the page on which he was before getting onto the Login page
+    res.redirect(redirectUrl);  //we're redirecting the user to the page on which he was before getting onto the Login page with the help of sessions in middleware.js
 };
-//By using the storeReturnTo middleware function, we can save the req.session.returnToUrlBeforeLogIn value to res.locals before passport.authenticate() clears the session 
-//and deletes 'req.session.returnToUrlBeforeLogIn'. This enables us to access and use the req.session.returnToUrlBeforeLogIn value (via res.locals.returnToUrlBeforeLogIn).
-//later in the middleware chain so that we can redirect users to the appropriate page after they have logged in.
+
 
 module.exports.logoutUser = (req, res, next) => {
-    req.logout(function (err) {      //req.logout is method of passport for the logout. 'req.logout' requires call back function as an argument.
+    req.logout(function (err) {    
         if (err) {
             return next(err)
         }
-        req.flash('success', 'Goodbye!!'); //and we'll execute the code to set a flash message and  redirect the user in the 'req.logout' argurment
+        req.flash('success', 'Goodbye!!'); 
         res.redirect('/campgrounds');
     });
 };
