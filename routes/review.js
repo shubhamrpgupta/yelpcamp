e1c594bb6961208e29b50aa67f1d3807e7fa434d
shubhamrpgupta <144988807+shubhamrpgupta@users.noreply.github.com>
2023-10-06 19:15:46 +0530

    Updated review.js

--- routes/review.js
+++ routes/review.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });  //mergeparams because express router keep params separately for the routes.
-const catchAsync = require('../Utils/catchAsync')   //this is to handle the async errors.
-const { isLoggedIn, validateReview, isReviewOwnerValid } = require('../middleware');  //we're checking if the user is logged in or not
+const catchAsync = require('../Utils/catchAsync')  
+const { isLoggedIn, validateReview, isReviewOwnerValid } = require('../middleware'); 
 const reviewController = require('../controllers/reviewControls');
 
 
