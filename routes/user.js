bd9cfd35fd6312249eb82e49d4791d66491f49ca
shubhamrpgupta <144988807+shubhamrpgupta@users.noreply.github.com>
2023-10-06 19:16:51 +0530

    Updated user.js

--- routes/user.js
+++ routes/user.js
@@ -6,18 +6,14 @@ const { storeReturnTo } = require('../middleware');
 const userController = require('../controllers/userControls')
 
 
-// app.get('/registerfakeuser', async (req, res) => {   //this is an example
-//     const newUser = new User({ email: 'xyz@gmail.com', username: 'zyx' });
-//     const registerNewUser = await User.register(newUser, 'anypassword');
-//     res.send(registerNewUser);
-// })
+
 
 router.get('/register', userController.renderRegisterNewUser);
 router.post('/register', catchAsync(userController.registerNewUserAndLoggedIn));
 router.get('/login', userController.renderLogInUser);
+
 const passportMiddlewareToLogin = passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' });
-//we are giving the passportMiddlewareToLogin for the authentication of the password. for the authentication we are using passport.
-//in the 'passport.authenticate', 'local' is the strategy. 'failureFlash & failureRedirect' are the options.
+
 router.post('/login', storeReturnTo, passportMiddlewareToLogin, userController.logInUserAndGetBackToOriginalUrl);
 router.get('/logout', userController.logoutUser);
 
