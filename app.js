54a93bf00ab972c0e8219ec03c48fea3798b5de9
shubhamrpgupta <144988807+shubhamrpgupta@users.noreply.github.com>
2023-10-06 18:58:50 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -2,32 +2,27 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-const express = require('express');   //With the help of EJS, we create and use different or multiple views. 
-// In views, Either we could have EJS files(URLs) or we can use this in JSON as well
+const express = require('express');  
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const campgroundRoute = require('./routes/campground');
 const reviewRoute = require('./routes/review');
 const userRoute = require('./routes/user');
-const session = require('express-session');  // to use the session for the flash and authentication.
-const flash = require('connect-flash');  //to flash any required message
-const ExpressError = require('./Utils/ExpressError')  //this is to customised error handler
-const ejsMate = require('ejs-mate');  //we install this to use a common EJS(Boilerplate)'s layout option to all the views ejs
-const methodOverride = require('method-override');   //We install this for the EDIT or UPDATE, 
-//because we have to override the method [from (GET/POST) to (PATCH/PUT/ETC)] in <a> in the ejs file
+const session = require('express-session'); 
+const flash = require('connect-flash');
+const ExpressError = require('./Utils/ExpressError')  
+const ejsMate = require('ejs-mate');  
+const methodOverride = require('method-override'); 
 const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');   //this is to ensure there is no sequential injection error taking pLce, 
-//security of 'req.body,req.querry,req.params etc' in the mongoose database.
-const passport = require('passport'); // to authenticate & hash, salt and store the username and password for any user.
-const localStrategy = require('passport-local');  //we are using the strategy know as 'Local', we can choose the different strategy from the passport website.
+const mongoSanitize = require('express-mongo-sanitize');   
+const passport = require('passport');
+const localStrategy = require('passport-local');  
 const User = require('./models/user');
-const MongoStore = require('connect-mongo'); //storage for the sessions's data in mongo. the default storage for sessions is localDatabse.
-//but to deploy the app, we use the mongo foe the sessions.
+const MongoStore = require('connect-mongo');
 
 
 const ourMongoDbUrl = process.env.MONGO_DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
-// process.env.MONGO_DB_URL
 
 main().catch(err => console.log(`Mongo ERROR, ${err}`));
 async function main() {
@@ -36,13 +31,13 @@ async function main() {
 }
 
 
-app.set('view engine', 'ejs');   // we put this to initiate EJS property 
-app.set('views', path.join(__dirname, 'views'))  // this is to launch the site or app successfuly within any directory
+app.set('view engine', 'ejs');  
+app.set('views', path.join(__dirname, 'views')) 
 
 
 const ourSecret = process.env.SECRET || 'notagoodsecret';
 
-const ourMongoStore = MongoStore.create({  //creating a new MongoStore for the sessions to be used.
+const ourMongoStore = MongoStore.create({  
     mongoUrl: ourMongoDbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
@@ -55,57 +50,48 @@ ourMongoStore.on('error', function (e) {
 });
 
 
-const sessionConfig = {  //for the argument of session app.use
+const sessionConfig = { 
     store: ourMongoStore,
-    name: 'sessionName',  //'connect.sid' is the default name.
+    name: 'sessionName', 
     secret: ourSecret,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        httpOnly: true,  //for the security reasons we give true which is default.
-        // secure:true,   //for the https
+        httpOnly: true,  
+        // secure:true,   //for the https requests
         expires: Date.now() + (1000 * 60 * 60 * 24 * 7),
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 
 
-app.use(mongoSanitize());  //to secure our mongoose databse throught the 'req.body, req.params, req.querry and etc' and we've to use this before.
-app.use(session(sessionConfig)) //for the sessions 
+app.use(mongoSanitize());  
+app.use(session(sessionConfig))  
 app.use(flash());
-app.use(passport.initialize()); //this middleware is require to start the passport. 
-app.use(passport.session());  //this command should be come after the app.use(session());
-//because we are going to have persistent login sessions or want use all the secret routes and won't need to login again and again for the same user.
+app.use(passport.initialize());  
+app.use(passport.session()); 
 passport.use(new localStrategy(User.authenticate()));
-// here we are saying passport to use strategy Local and authentication method is 'authenticate' which is located in our model 'user'
-// 'authenticate' static method has been added in our 'user' model with the help of 'passportLocalMongoose' and got plugin
-passport.serializeUser(User.serializeUser()); //again this is coming from the passportLocalMongoose.
-// serializeUser is how we store the user's data(it could be either _id or etc) into the session which be helpful for the login Once only.
-passport.deserializeUser(User.deserializeUser()); //this is to destroy the saved session. or for the logout.
-app.use((req, res, next) => {  //an express middleware for the flash.  we used app.use for all the sites.
-    res.locals.success = req.flash('success');     //In Express.js, res.locals is an object that provides a way to pass data through the application during the request-response cycle. 
-    res.locals.update = req.flash('update');  //It allows you to store variables that can be accessed by your templates and other middleware functions.
-    res.locals.erase = req.flash('erase'); //this 'locals' gives access the data in the global templates.
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
+app.use((req, res, next) => { 
+    res.locals.success = req.flash('success');    
+    res.locals.update = req.flash('update');  
+    res.locals.erase = req.flash('erase');
     res.locals.error = req.flash('error');
-    res.locals.signedInUser = req.user;   //we're using this info in navbar file.
-    //with the help of passport we are getting the user's data who is signed in from the 'req.user'
-    //and we are saving the user's data in every page. we're saving it in locals.signedInUser
-    //now we'll have the user's data and we can show that in the navbar's ejs. and in every template. because it's in global app template.
+    res.locals.signedInUser = req.user;  
     next();
 })
-//the passport.serializeUser()  and passport.deserializeUser()  actions need to occur before trying to access req.user, as the user info is not introduced into the request until then.
-//Also, the middleware for the locals, needs to be defined above the routes(routes folder) in order for the routes (and by extension, the EJS templates) to have access to res.locals.
 
-app.use(express.urlencoded({ extended: true }));   // To  use the request data in URL format from the POST type request. or to parse the data into POST
-app.use(express.json());   // To  use the request data in JSON format from the POST type request
+app.use(express.urlencoded({ extended: true }));   
+app.use(express.json());   
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'staticFiles')))  //static is use for the files which will not change or for the public files
+app.use(express.static(path.join(__dirname, 'staticFiles')))  
 app.use('/campgrounds', campgroundRoute);
 app.use('/campgrounds/:id/review', reviewRoute);
 app.use('/', userRoute);
 
 
-app.engine('ejs', ejsMate);  // to use Boilerplate EJS as a structure for all the views EJS
+app.engine('ejs', ejsMate); 
 
 
 app.use(helmet());
