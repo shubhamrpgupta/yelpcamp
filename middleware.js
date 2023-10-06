748aeaa588da8cc027d38f528ea809ab95f30fd5
shubhamrpgupta <144988807+shubhamrpgupta@users.noreply.github.com>
2023-10-06 19:00:33 +0530

    Updated middleware.js

--- middleware.js
+++ middleware.js
@@ -1,13 +1,9 @@
-//to use this middleware for multiple routes or for multiple models, we are storing the middleware in the separate file.
-//we cannot put this middleware in the app.js because we have to trim the app.js
 const Campground = require('./models/campground');
 const Review = require('./models/review');
-const { campgroundSchema, reviewSchema } = require('./schemas'); //for the validateCampground and validateReview
-const ExpressError = require('./Utils/ExpressError'); //this is to customised error handler
+const { campgroundSchema, reviewSchema } = require('./schemas'); 
+const ExpressError = require('./Utils/ExpressError'); 
 
 
-//if user wants to add a new campground or edit or etc they should have logged in first
-// 'req.isAuthenticated' is the passport's method to check the authenticity of user.
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnToUrlBeforeLogIn = req.originalUrl;
@@ -17,7 +13,7 @@ module.exports.isLoggedIn = (req, res, next) => {
         next();
 }
 
-//if the owner is valid to edit or delete the campground with all the data in it. otherwise we'll hide the buttons.
+
 module.exports.isOwnerValid = async (req, res, next) => {
     const { id } = req.params
     const foundCamp = await Campground.findById(id);
@@ -28,7 +24,7 @@ module.exports.isOwnerValid = async (req, res, next) => {
     next()
 }
 
-//if the review's Owner is valid to delete the review.
+
 module.exports.isReviewOwnerValid = async (req, res, next) => {
     const { id, reviewId } = req.params
     const foundReview = await Review.findById(reviewId);
@@ -39,19 +35,18 @@ module.exports.isReviewOwnerValid = async (req, res, next) => {
     next()
 }
 
-//if the new or updated campground is in the required format from all the end.
-module.exports.validateCampground = (req, res, next) => {   //check the schemas.js file
-    const { error } = campgroundSchema.validate(req.body);    //to get any error during the data input for the new campground,
-    if (error) {    //we are showing the error on the server site,
-        const msg = error.details.map(el => el.message).join(',')   //to show the error, We are using this format because,
-        throw new ExpressError(msg, 400)   // the result which we are getting from the campgroundSchema.validate(req.body) is an ARRAY
-        // so we took error from the RESULT ARRAY and map that because that is an array and it could be more than one. for that we have to join all the messages.
+
+module.exports.validateCampground = (req, res, next) => {   
+    const { error } = campgroundSchema.validate(req.body);   
+    if (error) {   
+        const msg = error.details.map(el => el.message).join(',')   
+        throw new ExpressError(msg, 400)   
     }
     next();
 }
 
 module.exports.validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);  //we are using mongoose middleware which is 'validate'
+    const { error } = reviewSchema.validate(req.body);  
     console.log(error)
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
@@ -60,10 +55,7 @@ module.exports.validateReview = (req, res, next) => {
     next();
 }
 
-//'storeReturnTo' to transfer the returnToUrlBeforeLogIn value from the session (req.session.returnToUrlBeforeLogIn)
-//to the Express.js app res.locals object before the passport.authenticate() function is executed in the /login POST route.
-//In Express.js, res.locals is an object that provides a way to pass data through the application during the request-response cycle.
-// It allows you to store variables that can be accessed by your templates and other middleware functions.
+
 module.exports.storeReturnTo = (req, res, next) => {
     if (req.session.returnToUrlBeforeLogIn) {
         res.locals.returnToUrlBeforeLogIn = req.session.returnToUrlBeforeLogIn;
